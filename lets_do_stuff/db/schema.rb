65ba5016e45c4ddae49eef0ae5b87cd85edbdca8
Vishal Kalyanasundaram <vishalkal@berkeley.edu>
2015-10-23 15:10:48 -0700

    Migrates user field, generates controller

--- lets_do_stuff/db/schema.rb
+++ lets_do_stuff/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022235703) do
+ActiveRecord::Schema.define(version: 20151023220822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,11 @@ ActiveRecord::Schema.define(version: 20151022235703) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.integer  "age"
+    t.string   "location"
+    t.text     "hobbies"
+    t.text     "about_me"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
