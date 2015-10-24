473d72edd8063c92511faedbbaa5657b86091616
Victoria <victorialo@berkeley.edu>
2015-10-24 02:32:32 +0000

    moved all files outside

---
+++ app/models/room.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: rooms
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Room < ActiveRecord::Base
+    belongs_to :category
+end
