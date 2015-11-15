70487fad29c8c41b1a5453cd64e2ffe4bb5e0104
zhuosi <wangzhuosi13@hotmail.com>
2015-11-14 21:59:39 -0800

    when the user haven't set his name, we should not display xx's homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @categories = Category.all
    if current_user.present?
-	   if current_user.name == "name"
+	   if current_user.name == "Name" or current_user.name == nil
 	   		@name = ""
 	   	else
 	   		@name = current_user.name.to_s + "'s Page"
