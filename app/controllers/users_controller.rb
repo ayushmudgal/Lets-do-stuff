6782f7784b4fd4c9801a0cf5be2be1796365d121
Ayush Mudgal <ayushincnca@gmail.com>
2015-11-17 10:24:25 -0800

    fixed the bug set_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -82,7 +82,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(current_user.id)
+      @user = User.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
