540f1e9cebe3bb7d81c5c28960a1b3be0507db4b
Bofan Chen <bofanchen1@gmail.com>
2015-11-17 21:11:47 -0800

    added about on welcome page, but still needs styling

--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,13 @@ ActiveRecord::Schema.define(version: 20151117033928) do
 
   add_index "rooms", ["category_id"], name: "index_rooms_on_category_id", using: :btree
 
+  create_table "userRooms", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "model_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "user_rooms", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "room_id"
