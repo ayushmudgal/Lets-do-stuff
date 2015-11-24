59749febce23dab158eabc5cc9612a3f544db4c3
Bofan Chen <bofanchen1@gmail.com>
2015-11-23 21:03:04 -0800

    set time zone to PST

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module LetsDoStuff
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
+    config.time_zone = 'Pacific Time (US & Canada)'
   end
 end
