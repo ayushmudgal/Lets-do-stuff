ff5f641e446f3864fa47f8fd8df620d24d5d84fa
zhuosi <wangzhuosi13@hotmail.com>
2015-11-23 19:48:37 -0800

    Get ride of the head line on the main page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,5 @@
 class WelcomeController < ApplicationController
   def index
     @categories = Category.all
-   if current_user.present?
-	   if current_user.name == "Name" or current_user.name == nil
-	   		@name = ""
-	   	else
-	   		@name = current_user.name.to_s + "'s Page"
-	  	end
-	  end
 	end
 end
