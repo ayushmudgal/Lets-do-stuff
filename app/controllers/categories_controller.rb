efe4780b7c857bf32e2585763d1b32798d49f68e
Ayush Mudgal <ayushincnca@gmail.com>
2015-11-23 21:25:56 -0800

    No one can edit any cateogries anymore

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_filter :require_user, only: [:edit]
 
   # GET /categories
   # GET /categories.json
@@ -74,6 +75,11 @@ class CategoriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
+    end
+
+     def require_user
+      flash[:notice] = "Sorry, you are not authorized to access to this page!"
+      redirect_to room_path
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
