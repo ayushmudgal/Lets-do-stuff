81058efb4aee65fcea42c3c22e5519040efe92ae
Ayush Mudgal <ayushincnca@gmail.com>
2015-11-23 23:07:53 -0800

    Added scenario feature tests

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,46 +2,35 @@ require "rails_helper"
 require "pp"
 
 feature "All Features Test" do
-	scenario "check if it direct to new login page for hardlink to new pin path" do
-    visit pins_new_path  
-    expect(current_path).to eq "/login"
-  end
-
-	scenario("check if u can access new user link from home") do
-    visit root_path
-    click_link('Signupbutton')
-    expect(page).to have_content "SIGN UP"
-  end    
-
- scenario "create valid new user" do 
-   user = FactoryGirl.build_stubbed(:user, email: "ki.ey.kouch@berkeley.edu")
-   visit "/signup"     
-   fill_in "user_username", with: user.username
-   fill_in "user_password", with: user.password
-   fill_in "user_email", with: user.email   
-   click_button "Create an account"
+	
+ scenario "Create Good New User" do 
+   visit "/users/sign_up"     
+   fill_in "Name", with: "Ayush"
+   fill_in "Password", with: "12345678", match: :prefer_exact
+   fill_in "Password confirmation", with: "12345678", match: :prefer_exact
+   fill_in "Email", with: "ayushincnca@gmail.com"
+   click_button "Sign up"
    expect(current_path).to eq "/"
  end
 
  scenario "invalid email new user" do
    user = FactoryGirl.build_stubbed(:user) 
    visit root_path
-   visit "/signup"
+   visit "users/sign_up"
       
-   fill_in "user_username", with: user.username
-   fill_in "user_password", with: user.password
-   fill_in "user_email", with: user.email   
-   click_button "Create an account"    
-
-   expect(current_path).to eq "/signup"
+   fill_in "Name", with: user.name
+   fill_in "Password", with: user.password, match: :prefer_exact
+   fill_in "user_email", with: "ayushincnca@berkeley.edu"   
+   click_button "Sign up"    
+   expect(current_path).to eq "/users"
  end
 
- scenario "valid login" do
-   user = FactoryGirl.create(:user, email: "ki.ey.kouch@berkeley.edu")
-   visit "/login"
-   fill_in "session_email", :with => user.email
-   fill_in "session_password", :with => user.password
-   click_button "Continue"
-   expect(page).to have_content("Welcome to Pinterview")
+ scenario "invalid sign in" do
+   visit root_path 
+   visit "/users/sign_in"
+   fill_in "Email", with: "ayushincnca@gmail.com"
+   fill_in "Password", with: "1234"
+   click_button "Log in"
+   expect(page).to have_content("Log in")
  end
 end
